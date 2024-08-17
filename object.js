35c1d66f081d16b52e20854e316d5ee6afd6888a
Dynno Yohanis Ottu <dynnoottu968@gmail.com>
2024-08-17 22:39:45 +0700

    Step awal belajar js oop

---
+++ object.js
@@ -0,0 +1,14 @@
+function NamaKotaDanProvinsi() {
+  this.kota = "";
+  this.provinsi = "";
+}
+
+const result1 = new NamaKotaDanProvinsi();
+result1.kota = "Kota Kupang";
+result1.provinsi = "Nusa Tenggara Timur";
+console.log(result1);
+
+const result2 = new NamaKotaDanProvinsi();
+result2.kota = "Kota Bandung";
+result2.provinsi = "Jawa Barat";
+console.log(result2);
